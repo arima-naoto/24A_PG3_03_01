2ec26ac34eede27ef4b5dddea09087babe65c0ee
arima-naoto <k023g1010@g.neec.ac.jp>
2024-05-17 15:41:48 +0900

    PG3_03_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,66 @@
-#include <stdio.h>
+#include <iostream>
+#include <List>
+#include <string>
+using namespace std;
 
 int main() 
 {
+	//山手線の駅名を全て英語で宣言する
+	list<string> stationName = {
+		"ToKyo","Kanda","Akihabara","OkaChiMachi","Ueno",
+		"Uguisudani","Nippori","Tabata","Komagome","Sugamo"
+		"Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo",
+		"Shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu",
+		"Meguro","Gotanda","Osaki","Shinagawa","Tamati",
+		"HamamatsuCho","Shimbashi","Yarakucho"
+	};
+
+	cout << "1970年の山手線の駅一覧\n";
+	//1970年時点での山手線の駅をfor分を使用して一つずつアクセスしていく
+	for (auto itr = stationName.begin(); itr != stationName.end(); ++itr) 
+	{
+		cout << *itr << "\n";
+	}
+
+	//任意の位置に要素を追加する処理
+	for (list<string>::iterator itr = stationName.begin(); itr != stationName.end(); ++itr) 
+	{
+		//インテレータが田端駅だった場合
+		if (*itr == "Tabata") 
+		{
+			//田町駅の前に西日暮里駅を追加する
+			itr = stationName.insert(itr, "Nishi-Nippori");
+			//インテレータをインクリメントする
+			itr++;
+		}
+	}
+
+	cout << "\n2019年の山手線の駅一覧\n";
+	//2019年時点での山手線の駅をfor分を使用して一つずつアクセスしていく
+	for (auto itr = stationName.begin(); itr != stationName.end(); ++itr)
+	{
+		cout << *itr << "\n";
+	}
+
+	//任意の位置に要素を追加する処理
+	for (list<string>::iterator itr = stationName.begin(); itr != stationName.end(); ++itr)
+	{
+		//インテレータが田町駅だった場合
+		if (*itr == "Tamati")
+		{
+			//田町駅の前に高輪ゲートウェイを追加する
+			itr = stationName.insert(itr, "Takanawa-Gateway");
+			//インテレータをインクリメントする
+			itr++;
+		}
+	}
+
+	cout << "\n2022年の山手線の駅一覧\n";
+	//2022年時点での山手線の駅をfor分を使用して一つずつアクセスしていく
+	for (auto itr = stationName.begin(); itr != stationName.end(); ++itr)
+	{
+		cout << *itr << "\n";
+	}
+
 	return 0;
 }
